ead699b7a6a035f76a70a71b9511769a475f6c6e
veselin valkanov <veselin.d.valkanov@gmail.com>
2023-08-25 19:32:43 +0300

    random generator test

---
+++ testRandom/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace testRandom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Random random = new Random();
+                double smallWin = random.NextDouble() * 2.0;
+                double bigWin = random.NextDouble() * (10.0 - 2.0) + 2.0;
+                Console.WriteLine(smallWin);
+                Console.WriteLine(bigWin);
+                if(bigWin < 2.00 || bigWin > 10.00)
+                {
+                    Console.WriteLine("WROOOONG");
+                } 
+            }
+
+        }
+    }
+}
