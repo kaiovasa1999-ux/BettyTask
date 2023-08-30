575b36de51834a67fefc72b4ef52e545a78a28c9
veselin valkanov <veselin.d.valkanov@gmail.com>
2023-08-30 09:39:07 +0300

    I added second solution variant, it was a little bit more well structured and OOP oriented

---
+++ Task-V2/ConsoleInputDataValidator.cs
@@ -0,0 +1,92 @@
+﻿namespace Task_V2
+{
+    public class ConsoleInputDataValidator
+    {
+        private string[] validCommands = { "exit", "bet", "deposit", "withdrawal" };
+       
+        public bool ValidateInput(string[] input)
+        {
+            bool validSize = ValidateInputSize(input);
+            bool result = false;
+
+            if (validSize)
+            {
+                string cmd = GetCommand(input);
+                decimal amount = GetAmount(input);
+                bool isValidCommand = ValidateCommand(cmd, validCommands);
+                bool isValidAmount = ValidateAmount(amount);
+
+                if(cmd == "withdrawal")
+                {
+                    bool isvalidWithdrawal = ValidateWithdrawalAmount(cmd, amount);
+                    if (!isvalidWithdrawal)
+                    {
+                        return false;
+                    }
+                }
+
+                if (isValidCommand && isValidAmount && input.Count() <= 2)
+                {
+                  
+
+                    return true;
+                }
+            }
+          
+            return result;
+        }
+
+        public bool ValidateCommand(string cmd, string[] validCommands)
+        {
+            bool result = validCommands.Contains(cmd);
+            if (!result)
+            {
+                Console.WriteLine($"invalid command or amount. Please provide beteween 1 and 10 and commands should be {string.Join(", ", validCommands)}");
+            }
+            return result;
+        }
+        public bool ValidateAmount(decimal amount)
+        {
+            if (amount >= 1 && amount <= 10)
+            {
+                return true;
+            }
+            Console.WriteLine($"Please provide us amount between 1 and 10");
+            return false;
+        }
+
+        public bool ValidateWithdrawalAmount(string cmd, decimal amount)
+        {
+            bool result = false;
+            if(cmd == "withdrawal" && amount >= 1)
+            {
+                result = true;
+            }
+            return result;
+        }
+
+        public string GetCommand(string[] input)
+        {
+            if (input.Length == 0 || input.Length > 2)
+            {
+                Console.WriteLine("Invalid input");
+            }
+            return input[0];
+        }
+        public bool ValidateInputSize(string[] input)
+        {
+            bool result = false;
+            if (input.Count() > 0 || input.Count() < 2)
+            {
+                result = true;
+            }
+            return result;
+        }
+        public decimal GetAmount(string[] input)
+        {
+            decimal amount;
+            decimal.TryParse(input[1], out amount);
+            return amount;
+        }
+    }
+}
---
+++ Task-V2/Program.cs
@@ -0,0 +1,103 @@
+﻿namespace Task_V2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal balance = 0;
+            while (true)
+            {
+                Console.WriteLine("Pleasem submit action:");
+                string inputCommand = Console.ReadLine();
+                string[] dataIntput = inputCommand.Split(" ");
+                ConsoleInputDataValidator consoleInputDataValidator = new ConsoleInputDataValidator();
+                bool isInputValid = consoleInputDataValidator.ValidateInput(dataIntput);
+                if (isInputValid)
+                {
+                    string command = consoleInputDataValidator.GetCommand(dataIntput);
+                    decimal amount = consoleInputDataValidator.GetAmount(dataIntput);
+                    if (command == "deposit")
+                    {
+                        balance += amount;
+                        Console.WriteLine($"Your deposit of ${amount} was successful. Your current balance is: ${balance}");
+                        continue;
+                    }
+                    if (command == "withdrawal")
+                    {
+                        if (amount <= balance)
+                        {
+                            decimal left = balance -= amount;
+                            Console.WriteLine($"Your withdrawal of {amount}. Your current balance is: ${Math.Round(left, 2)}");
+                            Console.WriteLine("If you want to continue just press enter otherwise type 'exit'");
+                            var secondInput = Console.ReadLine();
+                            if (secondInput == "exit")
+                            {
+                                Console.WriteLine("Thank you for playing! Hope to see you again soon.");
+                                break;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Good choice!!!");
+                                continue;
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("NOT ENOUGH BALANCE!!!");
+                        }
+                        continue;
+                    }
+
+                    if (command == "bet" && amount <= balance)
+                    {
+                        balance = BetCalculation(amount,balance);
+                    }
+                }
+            }
+        }
+        private static decimal BetCalculation(decimal amount,decimal balance)
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(1, 101);
+
+            int smallWinChance = 50;
+            int bigWinChance = 10;
+
+            double smallWin = random.NextDouble() * 2.0;
+            double bigWin = random.NextDouble() * (10.0 - 2.0) + 2.0;
+            if (randomNumber > smallWinChance)
+            {
+                balance -= amount;
+                Console.WriteLine($"No luck this time! Your current balance is: ${Math.Round(balance, 2)}");
+            }
+
+            else
+            {
+
+                int decimalPlaces = 2;
+                decimal multiplier = (decimal)Math.Pow(10, decimalPlaces);
+                decimal winMoney;
+                decimal roundedWinMoney;
+                decimal realProfit;
+                randomNumber = 11;
+                if (randomNumber <= smallWinChance && randomNumber > bigWinChance)
+                {
+                    winMoney = amount * (decimal)smallWin;
+                    roundedWinMoney = Math.Floor(winMoney * multiplier) / multiplier;
+                    realProfit = Math.Abs(roundedWinMoney - amount);
+                    balance += realProfit;
+                    Console.WriteLine($"Congrats SMALL JAK - you won {Math.Round(realProfit, 2)}.Your current balance is: ${Math.Round(balance, 2)}");
+                }
+                if (randomNumber <= bigWinChance)
+                {
+                    winMoney = amount * (decimal)bigWin;
+                    roundedWinMoney = Math.Floor(winMoney * multiplier) / multiplier;
+                    realProfit = Math.Abs(roundedWinMoney - amount);
+                    balance += realProfit;
+                    Console.WriteLine($"Congrats BIG JAK - you won {realProfit:f2}.Your current balance is: ${Math.Round(balance, 2)}");
+                }
+            }
+            return balance;
+        }
+    }
+}
